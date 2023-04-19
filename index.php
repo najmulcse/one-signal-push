c3ba18f2b88c05cc2b485f45f512dcb35240feb1
Najmul Ahmed <najmul@wearenext.io>
2023-04-19 09:56:13 +0600

    initial

---
+++ index.php
@@ -0,0 +1,11 @@
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "f568b755-eb73-4aeb-aa64-193561696034",
+    });
+  });
+</script>
+
+<h2> One Signal Push Notification</h2>
